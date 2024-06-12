1cd6ca96ca721a27fc53b81d2b284e1b8fa90e30
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-06-13 01:01:20 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let answer = '';
+    
+    [...my_string].forEach(str => {
+        if (!answer.includes(str)) {
+            answer += str;
+        }
+    })
+    
+    return answer;
+}
