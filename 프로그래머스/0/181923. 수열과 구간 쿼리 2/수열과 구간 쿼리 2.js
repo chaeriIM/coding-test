b8cd21dab1c7de83a57f775516c95b182e4f180e
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-06-14 16:51:26 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 15.22 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,12 @@
+function solution(arr, queries) {
+    let result = [];
+    
+    for(let [s, e, k] of queries){
+        let temp = arr
+            .filter((v, i) => (i >= s && i <= e && v > k))
+            .sort((a, b) => a - b)[0];
+        result.push(temp ? temp : -1);
+    }
+    
+    return result;
+}
