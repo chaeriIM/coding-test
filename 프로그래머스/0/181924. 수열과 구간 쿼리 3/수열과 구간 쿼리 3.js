d52ba36ba283323a782175ec66f5144fc7cfcc36
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-06-14 15:41:43 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.43 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,7 +1,10 @@
 function solution(arr, queries) {
-    for (let i=0; i<queries.length; i++) {
-        const [a, b] = queries[i];
-        [arr[a], arr[b]] = [arr[b], arr[a]];
-    }
+    // for (let i=0; i<queries.length; i++) {
+    //     const [a, b] = queries[i];
+    //     [arr[a], arr[b]] = [arr[b], arr[a]];
+    // }
+    // return arr;
+    
+    queries.forEach(([a, b]) => [arr[a], arr[b]] = [arr[b], arr[a]]);
     return arr;
 }
