caba42d759f5fe1587a9386ac65fed720752947e
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-06-17 14:40:13 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,11 +1,3 @@
 function solution(s1, s2) {
-    let count = 0;
-    
-    s1.forEach(v => {
-        for (let i=0; i<s2.length; i++) {
-            if (s2[i] === v) count++;
-        }
-    })
-    
-    return count;
+    return s1.filter(v => s2.includes(v)).length;
 }
