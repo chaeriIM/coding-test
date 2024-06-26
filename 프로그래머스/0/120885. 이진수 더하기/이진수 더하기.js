26df41a21c2b8e42dfccca07c54080d488b2042c
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-06-26 23:32:44 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,5 +1,3 @@
 function solution(bin1, bin2) {
-    const decimal = parseInt(bin1, 2) + parseInt(bin2, 2);
-    
-    return decimal.toString(2);
+    return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
 }
