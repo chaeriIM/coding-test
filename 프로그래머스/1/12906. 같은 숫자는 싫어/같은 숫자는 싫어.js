ca728fe8f6b291c5aaa528e6eac0f6989cc85e32
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-07-04 00:52:13 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.23 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    let result = [];
+    
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i - 1] !== arr[i]) {
+            result.push(arr[i]);
+        }
+    }
+    
+    return result;
+}
