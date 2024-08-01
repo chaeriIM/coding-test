0ab3b4595e89509db81cdd0c5d291ae5061a01d1
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-08-01 18:45:47 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 5.81 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -0,0 +1,13 @@
+function solution(my_string, queries) {
+    let str = [...my_string];
+    
+    queries.forEach(([s, e]) => {
+        while (s < e) {
+            [str[s], str[e]] = [str[e], str[s]];
+            s++;
+            e--;
+        }
+    });
+    
+    return str.join('');
+}
