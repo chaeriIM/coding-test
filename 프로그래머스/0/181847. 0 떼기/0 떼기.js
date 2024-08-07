c1ecb0d82ba6718c9d92b3938abad263e507c66b
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-08-07 18:09:51 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -1,9 +1,11 @@
 function solution(n_str) {
-    const arr = [...n_str];
+//     const arr = [...n_str];
     
-    while (arr[0] === '0') {
-        arr.shift();
-    }
+//     while (arr[0] === '0') {
+//         arr.shift();
+//     }
     
-    return arr.join('');
+//     return arr.join('');
+    
+    return String(Number(n_str));
 }
