9de845b2f5779802584fa9f41a7d03ad00b4bfc2
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-08-09 17:39:17 +0900

    [level 0] Title: 세로 읽기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,9 +1,10 @@
 function solution(my_string, m, c) {
-    const arr = [];
+    let result = '';
     
     for (let i=0; i<my_string.length; i+=m) {
-        arr.push(my_string.slice(i, i+m));
+        const newStr = my_string.slice(i, i+m);
+        result += newStr[c-1];
     }
     
-    return arr.map(v => v[c-1]).join('');
+    return result;
 }
