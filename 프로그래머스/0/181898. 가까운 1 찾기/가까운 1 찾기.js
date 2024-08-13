24f7c1fb32aa64524e5ed76012d2e42739fa5c6c
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-08-13 19:22:03 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,8 +1,10 @@
 function solution(arr, idx) {
-    for (let i=idx; i<arr.length; i++) {
-        if (arr[i] === 1) {
-            return i;
-        }
-    }
-    return -1;
+    // for (let i=idx; i<arr.length; i++) {
+    //     if (arr[i] === 1) {
+    //         return i;
+    //     }
+    // }
+    // return -1;
+    
+    return arr.indexOf(1, idx);
 }
