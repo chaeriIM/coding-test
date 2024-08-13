d3306c6dc272e1d183cc67bf947c1aa7ec0f1203
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-08-13 19:08:31 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,6 +1,5 @@
 function solution(binomial) {
-    const arr = binomial.split(' ');
-    const [x, op, y] = arr;
+    const [x, op, y] = binomial.split(' ');
     
     if (op === "+") return Number(x) + Number(y);
     if (op === "-") return x - y;
