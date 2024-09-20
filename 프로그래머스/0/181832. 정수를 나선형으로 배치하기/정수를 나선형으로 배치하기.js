2a2efacd407782181ba4bd9a2905f6fb471a6d8c
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-09-20 23:33:02 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -0,0 +1,33 @@
+function solution(n) {
+    const arr = Array.from({ length: n }, () => Array(n).fill(0));
+    
+    let num = 1;
+    let top = 0;
+    let bottom = n - 1;
+    let left = 0;
+    let right = n - 1;
+    
+    while (top <= bottom && left <= right) {
+        for (let i=left; i<=right; i++) {
+            arr[top][i] = num++;
+        }
+        top++;
+        
+        for (let i=top; i<=bottom; i++) {
+            arr[i][right] = num++;
+        }
+        right--;
+        
+        for (let i=right; i>=left; i--) {
+            arr[bottom][i] = num++;
+        }
+        bottom--;
+        
+        for (let i=bottom; i>=top; i--) {
+            arr[i][left] = num++;
+        }
+        left++;
+    }
+    
+    return arr;
+}
