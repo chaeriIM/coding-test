693559f3ba20ff162ad2383e78d71176f193131e
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-09-25 23:13:07 +0900

    [level 1] Title: K번째수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+    let result = [];
+    
+    commands.forEach(([i, j, k]) => {
+        const arr = array.slice(i-1, j).sort((a, b) => a - b);
+        result.push(arr[k-1]);
+    });
+    
+    return result;
+}
