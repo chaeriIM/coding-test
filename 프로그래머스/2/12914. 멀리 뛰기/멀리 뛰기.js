514b63eb922a5daa53d1ac5d2ca0af3cb691760c
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-10-15 14:39:21 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let a = 0;
+    let b = 1;
+    let result = 0;
+    
+    for (let i=1; i<=n; i++) {
+        result = (a + b) % 1234567;
+        a = b;
+        b = result;
+    }
+    
+    return result;
+}
