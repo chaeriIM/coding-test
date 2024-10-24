e5218725aa06a95b230d37ee05702b5f55ecbc87
chaeriIM <51882233+chaeriIM@users.noreply.github.com>
2024-10-25 00:35:47 +0900

    [level 1] Title: 카드 뭉치, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,16 @@
+function solution(cards1, cards2, goal) {
+    let one = 0;
+    let two = 0;
+    
+    for (let i=0; i<goal.length; i++) {
+        if (goal[i] === cards1[one]) {
+            one++;
+        } else if (goal[i] === cards2[two]) {
+            two++;
+        } else {
+            return "No";
+        }
+    }
+    
+    return "Yes";
+}
